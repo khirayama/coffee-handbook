2652f285032511474a208f2f0bd01ed9e5a4953e
Kotaro Hirayama <just.do.it.my.life@gmail.com>
2018-05-28 08:36:51 +0900

    Create sample posts

--- data/posts/2/meta.js
+++ data/posts/2/meta.js
@@ -1,14 +1,14 @@
 module.exports = {
-  id: 1,
+  id: 2,
   createdAt: '2018-05-26 12:00',
-  publishedAt: '2018-05-26 15:00',
+  publishedAt: '2018-05-27 15:00',
   thumbnailUrl: {
     ja: 'http://d3f3hpqbpy4ou2.cloudfront.net/production/7e8e8b9ee54e5f6380a7b378ee0bb0f8ebd5b93a62bdfadb785f2d039c275820.jpg?1477936056',
     en: 'http://d3f3hpqbpy4ou2.cloudfront.net/production/7e8e8b9ee54e5f6380a7b378ee0bb0f8ebd5b93a62bdfadb785f2d039c275820.jpg?1477936056',
   },
   title: {
-    ja: '【特集】なぜ僕らがこの活動をはじめたのか。',
-    en: '[Feature] Why we start this activities.',
+    ja: '【特集】なぜ僕らがこの活動をはじめたのか。2',
+    en: '[Feature] Why we start this activities. 2',
   },
   categories: [[1, null]],
   tags: [1],
--- data/posts/4/meta.js
+++ data/posts/4/meta.js
@@ -1,15 +1,15 @@
 module.exports = {
-  id: 1,
+  id: 4,
   createdAt: '2018-05-26 12:00',
-  publishedAt: '2018-05-26 15:00',
+  publishedAt: '2018-05-27 16:00',
   thumbnailUrl: {
     ja: 'http://d3f3hpqbpy4ou2.cloudfront.net/production/7e8e8b9ee54e5f6380a7b378ee0bb0f8ebd5b93a62bdfadb785f2d039c275820.jpg?1477936056',
     en: 'http://d3f3hpqbpy4ou2.cloudfront.net/production/7e8e8b9ee54e5f6380a7b378ee0bb0f8ebd5b93a62bdfadb785f2d039c275820.jpg?1477936056',
   },
   title: {
-    ja: '【特集】なぜ僕らがこの活動をはじめたのか。',
-    en: '[Feature] Why we start this activities.',
+    ja: '通常記事',
+    en: 'Regular',
   },
-  categories: [[1, null]],
-  tags: [1],
+  categories: [[3, null]],
+  tags: [],
 };
