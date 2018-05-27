6f648658de2b62d7b3fba0dba07213fe6f4d5153
Kotaro Hirayama <just.do.it.my.life@gmail.com>
2018-05-27 09:04:48 +0900

    Create data/dic and posts

---
+++ src/data/dic.js
@@ -0,0 +1,28 @@
+module.exports = {
+  nav: {
+    HOME: {
+      ja: 'HOME',
+      en: 'HOME',
+    },
+    COFFEE_BEANS: {
+      ja: 'COFFEE BEANS',
+      en: 'COFFEE BEANS',
+    },
+    BEVERAGES: {
+      ja: 'BEVERAGES',
+      en: 'BEVERAGES',
+    },
+    FOODS: {
+      ja: 'FOODS',
+      en: 'FOODS',
+    },
+    GOODS: {
+      ja: 'GOODS',
+      en: 'GOODS',
+    },
+    KNOWLEDGE: {
+      ja: 'KNOWLEDGE',
+      en: 'KNOWLEDGE',
+    },
+  },
+};
