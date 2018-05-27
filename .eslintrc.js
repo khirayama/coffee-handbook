05af4923893d84bf9ab9c56a919db9b64d67328e
Kotaro Hirayama <just.do.it.my.life@gmail.com>
2018-05-27 10:13:42 +0900

    Create config files for eslint, babel, postcss and webpack

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,16 +1,10 @@
 module.exports = {
-  extends: [
-    'eslint-config-xo-space/esnext',
-    'eslint-config-prettier',
-    'eslint-config-prettier/standard',
-  ],
+  extends: ['eslint-config-xo-space/esnext', 'eslint-config-prettier', 'eslint-config-prettier/standard'],
   env: {
+    browser: true,
     node: true,
   },
   rules: {
-    'comma-dangle': [
-      'error',
-      'always-multiline',
-    ],
-  }
+    'comma-dangle': ['error', 'always-multiline'],
+  },
 };
---
+++ webpack.config.js
@@ -0,0 +1,58 @@
+/* eslint-disable no-console */
+const path = require('path');
+const glob = require('glob');
+const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
+
+const NODE_ENV = process.env.NODE_ENV;
+const DIST = './src/assets';
+
+const inputFilenames = glob.sync(path.join(__dirname, 'src', 'scripts', '**', 'index.js'));
+const entry = {};
+for (let i = 0; i < inputFilenames.length; i++) {
+  const inputFilename = inputFilenames[i];
+  const outputFilename = inputFilename.replace(path.join(__dirname, 'src'), DIST).replace('index.js', 'bundle');
+  entry[outputFilename] = inputFilename;
+}
+
+const config = {
+  entry,
+  output: {
+    filename: '[name].js',
+    path: __dirname,
+  },
+  resolve: {
+    modules: ['src/scripts', 'node_modules'],
+    extensions: ['.js', '.json'],
+  },
+  plugins: [],
+  optimization: {
+    splitChunks: {
+      name: 'commons',
+      chunks: 'initial',
+    },
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader',
+      },
+    ],
+  },
+};
+
+if (NODE_ENV === 'production') {
+  console.log(`Building as production...`);
+  config.optimization.minimize = true;
+
+  if (process.env.ANALYSIS === 'true') {
+    console.log(`Building for analyze...`);
+    config.plugins.push(new BundleAnalyzerPlugin());
+  }
+} else {
+  console.log('Building as development...');
+  config.devtool = 'inline-source-map';
+}
+
+module.exports = config;
