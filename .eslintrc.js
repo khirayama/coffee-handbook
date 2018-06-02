e1a45a88425afc3e1fe389dd9300ce14b8831b05
Kotaro Hirayama <just.do.it.my.life@gmail.com>
2018-06-02 11:41:55 +0900

    Update eslint and prettier config

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,10 +1,12 @@
 module.exports = {
   extends: ['eslint-config-xo-space/esnext', 'eslint-config-prettier', 'eslint-config-prettier/standard'],
+  plugins: ['eslint-plugin-prettier'],
   env: {
     browser: true,
     node: true,
   },
   rules: {
+    'prettier/prettier': 'error',
     'comma-dangle': ['error', 'always-multiline'],
   },
 };
