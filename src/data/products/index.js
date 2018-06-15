41882a80a0dca6f1c2c583cce850c923f40fc736
Kotaro Hirayama <just.do.it.my.life@gmail.com>
2018-06-16 06:17:47 +0900

    Update sample product data

--- src/data/products/index.js
+++ src/data/products/index.js
@@ -1,98 +1,101 @@
-const products = [
-  {
-    key: 'hario-v60',
-    url: '/products/hario-v60',
-    category: {
-      ja: '抽出器具',
-      en: 'Brewers',
-    },
-    name: {
-      ja: 'ハリオ V60',
-      en: 'HARIO V60',
-    },
-    title: {
-      ja: 'ハリオ V60',
-      en: 'HARIO V60',
-    },
-    description: {
-      ja: 'コーヒー desc',
-      en: 'COFFEE desc',
-    },
-    thumbnailUrl: {
-      default: {
-        ja: '/images/image_1@square.jpg',
-        en: '/images/image_1@square.jpg',
-      },
-      square: {
-        ja: '/images/image_1@square.jpg',
-        en: '/images/image_1@square.jpg',
-      },
-      rectangle: {
-        ja: '/images/image_1@rectangle.jpg',
-        en: '/images/image_1@rectangle.jpg',
-      },
-    },
-    pictures: [
-      {
-        url: {
-          ja: '/images/image_1@square.jpg',
-          en: '/images/image_1@square.jpg',
-        },
-        caption: {
-          ja: 'キャプション 1',
-          en: 'caption 1',
-        },
-      },
-      {
-        url: {
-          ja: '/images/image_2@square.jpg',
-          en: '/images/image_2@square.jpg',
-        },
-        caption: {
-          ja: 'キャプション 2',
-          en: 'caption 2',
-        },
-      },
-      {
-        url: {
-          ja: '/images/image_3@square.jpg',
-          en: '/images/image_3@square.jpg',
-        },
-        caption: {
-          ja: 'キャプション 3',
-          en: 'caption 3',
-        },
-      },
-      {
-        url: {
-          ja: '/images/image_4@square.jpg',
-          en: '/images/image_4@square.jpg',
-        },
-        caption: {
-          ja: 'キャプション 4',
-          en: 'caption 4',
-        },
-      },
-      {
-        url: {
-          ja: '/images/image_5@square.jpg',
-          en: '/images/image_5@square.jpg',
-        },
-        caption: {
-          ja: 'キャプション 5',
-          en: 'caption 5',
-        },
-      },
-    ],
-    text: 'hogehoge',
-    colos: [],
-    links: [
-      {
-        site: 'Amazon',
-        url: '',
-      },
-    ],
+const harioV60 = {
+  key: 'hario-v60',
+  url: '/products/hario-v60',
+  category: {
+    ja: '抽出器具',
+    en: 'Brewers',
   },
-];
+  name: {
+    ja: 'ハリオ V60',
+    en: 'HARIO V60',
+  },
+  title: {
+    ja: 'ハリオ V60',
+    en: 'HARIO V60',
+  },
+  description: {
+    ja: 'コーヒー desc',
+    en: 'COFFEE desc',
+  },
+  thumbnailUrl: {
+    default: {
+      ja: '/images/image_1@square.jpg',
+      en: '/images/image_1@square.jpg',
+    },
+    square: {
+      ja: '/images/image_1@square.jpg',
+      en: '/images/image_1@square.jpg',
+    },
+    rectangle: {
+      ja: '/images/image_1@rectangle.jpg',
+      en: '/images/image_1@rectangle.jpg',
+    },
+  },
+  pictures: [
+    {
+      url: {
+        ja: '/images/image_1@square.jpg',
+        en: '/images/image_1@square.jpg',
+      },
+      caption: {
+        ja: 'キャプション 1',
+        en: 'caption 1',
+      },
+    },
+    {
+      url: {
+        ja: '/images/image_2@square.jpg',
+        en: '/images/image_2@square.jpg',
+      },
+      caption: {
+        ja: 'キャプション 2',
+        en: 'caption 2',
+      },
+    },
+    {
+      url: {
+        ja: '/images/image_3@square.jpg',
+        en: '/images/image_3@square.jpg',
+      },
+      caption: {
+        ja: 'キャプション 3',
+        en: 'caption 3',
+      },
+    },
+    {
+      url: {
+        ja: '/images/image_4@square.jpg',
+        en: '/images/image_4@square.jpg',
+      },
+      caption: {
+        ja: 'キャプション 4',
+        en: 'caption 4',
+      },
+    },
+    {
+      url: {
+        ja: '/images/image_5@square.jpg',
+        en: '/images/image_5@square.jpg',
+      },
+      caption: {
+        ja: 'キャプション 5',
+        en: 'caption 5',
+      },
+    },
+  ],
+  content: {
+    ja: '<h2>V60なんです</h2>',
+    en: '<h2>This is V60</h2>',
+  },
+  colos: [],
+  links: [
+    {
+      site: 'Amazon',
+      url: '',
+    },
+  ],
+};
+
+const products = [harioV60, harioV60, harioV60, harioV60];
 
 module.exports = products;
