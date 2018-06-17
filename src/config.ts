e68a84335953880b2ceda52536c063787d76a1e7
Kotaro Hirayama <just.do.it.my.life@gmail.com>
2018-06-17 22:05:35 +0900

    Migrate config.json to config.ts

---
+++ src/config.ts
@@ -0,0 +1,9 @@
+export const config = {
+  url: 'https://hirayamacoffee.com',
+  languages: ['ja', 'en'],
+  keywords: 'hirayama, 平山, coffee, コーヒー, 珈琲, institute, 研究所',
+  facebookAppId: 'test id',
+  facebookPageUrl: 'facebook page url',
+  twitterCardType: 'summary_large_image',
+  twitterAccount: '@hirayama_coffee_institute',
+};
