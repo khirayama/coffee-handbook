785eb8e56392b9809893da800ad357a3703c4cf2
Kotaro Hirayama <just.do.it.my.life@gmail.com>
2018-06-19 09:24:40 +0900

    Define good raw interface

--- src/data/goods.ts
+++ src/data/goods.ts
@@ -1,3 +1,69 @@
 import { harioV60 } from 'data/goods/harioV60';
 
-export const goods: any = [harioV60, harioV60, harioV60, harioV60];
+export interface IRawGood {
+  key: string;
+  url: string;
+  meta: {
+    title: {
+      ja: string;
+      en: string;
+    };
+    description: {
+      ja: string;
+      en: string;
+    };
+    thumbnailUrl: {
+      square: {
+        ja: string;
+        en: string;
+      };
+      rectangle: {
+        ja: string;
+        en: string;
+      };
+    };
+  };
+  category: {
+    ja: string;
+    en: string;
+  };
+  name: {
+    ja: string;
+    en: string;
+  };
+  pictures: {
+    url: {
+      ja: string;
+      en: string;
+    };
+    caption: {
+      ja: string;
+      en: string;
+    };
+  }[];
+  summary: {
+    ja: string;
+    en: string;
+  };
+  specs: {
+    name: {
+      ja: string;
+      en: string;
+    };
+    value: {
+      ja: string;
+      en: string;
+    };
+  }[];
+  content: {
+    ja: string;
+    en: string;
+  };
+  colors: string[];
+  links: {
+    site: string;
+    url: string;
+  }[];
+}
+
+export const goods: IRawGood[] = [harioV60, harioV60, harioV60, harioV60];
