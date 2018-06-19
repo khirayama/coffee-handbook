78bdad03e0bc7a115e4a437f109176e4609b79be
Kotaro Hirayama <just.do.it.my.life@gmail.com>
2018-06-20 08:15:37 +0900

    Refactor src/index.ts

--- src/index.ts
+++ src/index.ts
@@ -14,46 +14,37 @@ import { goodHandler } from 'handlers/goodHandler';
 import { goodsHandler } from 'handlers/goodsHandler';
 import { homeHandler } from 'handlers/homeHandler';
 import { postHandler } from 'handlers/postHandler';
-import { rss } from 'utils/rss';
+import { rssHandler } from 'handlers/rssHandler';
+import { setLang } from 'middlewares/setLang';
 
 const app: express = express();
 
-// Set middleware
+// Middleware
 const basedir: string = path.join(__dirname, 'presentations');
 app.locals.basedir = basedir;
 app.set('views', basedir);
 app.set('view engine', 'pug');
-app.use(
-  compression({
-    level: 9,
-  }),
-);
+app.use(compression({ level: 9 }));
 app.use(express.static(path.join(__dirname, 'assets')));
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(cookieParser());
-app.use((req: any, res: any, next: any) => {
-  let lang: string = req.query.lang || req.cookies.lang || config.languages[0];
-  if (config.languages.indexOf(lang) === -1) {
-    lang = config.languages[0];
-  }
-  res.cookie('lang', lang, { maxAge: 60000, httpOnly: false });
-  req.lang = lang;
-  next();
-});
+app.use(setLang);
 
 // Routing
 app
   .get('/', homeHandler)
-  .use('/beverages', new express.Router().get('/', beveragesHandler).get('/:beverageKey/:type', beverageHandler))
-  .use('/foods', new express.Router().get('/', foodsHandler).get('/:foodKey', foodHandler))
-  .use('/goods', new express.Router().get('/', goodsHandler).get('/:goodKey', goodHandler))
+  .get('/beverages', beveragesHandler)
+  .get('/beverages/:beverageKey/:recipeType', beverageHandler)
+  .get('/foods', foodsHandler)
+  .get('/foods/:foodKey', foodHandler)
+  .get('/goods', goodsHandler)
+  .get('/goods/:goodKey', goodHandler)
   .get('/about-us', aboutUsHandler)
   .get('/posts/:id', postHandler)
-  .get('/rss*', (req: any, res: any) => {
-    res.set('Content-Type', 'text/xml').send(rss[req.lang]);
-  });
+  .get('/rss*', rssHandler);
 
 // Server
 app.listen(3030, () => {
-  console.log(`Start app at ${new Date().toString()}.`); // tslint:disable-line:no-console
+  // tslint:disable-next-line:no-console
+  console.log(`Start app at ${new Date().toString()}.`);
 });
