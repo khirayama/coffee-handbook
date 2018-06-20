2d5cbe0f9eb90d4bf4128e407f56a4b70c9fe068
Kotaro Hirayama <just.do.it.my.life@gmail.com>
2018-06-21 07:52:07 +0900

    Support new menu template for foodsHandler

--- src/handlers/foodsHandler.ts
+++ src/handlers/foodsHandler.ts
@@ -1,38 +1,59 @@
+import * as express from 'express';
+
 import { config } from 'config';
-import { Recipe } from 'resources/Recipe';
+import { IMenuPage, IRecipeItemComponent } from 'presentations/templates/Menu';
+import { IRecipe, Recipe } from 'resources/Recipe';
 import { Dictionary } from 'utils/Dictionary';
 
-export function foodsHandler(req: any, res: any): void {
+export function foodsHandler(req: express.Request, res: express.Response): void {
   const dic: Dictionary = new Dictionary(req.lang);
-  const foodRecipes: any = Recipe(req.lang)
+  const foodRecipes: IRecipe[] = Recipe(req.lang)
     .where({
       category: dic.t('Templates.Foods.FOODS'),
     })
     .find();
-  const items: any[] = foodRecipes.map(
-    (foodRecipe: any): any => {
+  const items: IRecipeItemComponent[] = foodRecipes.map(
+    (foodRecipe: IRecipe): IRecipeItemComponent => {
       return {
         name: foodRecipe.name,
         hot: null,
         iced: null,
-        default: {
-          url: foodRecipe.url,
+        defaults: {
+          href: foodRecipe.url,
         },
       };
     },
   );
 
-  res.render('templates/Menu', {
-    config,
+  const props: IMenuPage = {
+    author: dic.t('author'),
+    name: dic.t('name'),
+    baseUrl: config.url,
+    facebookAppId: config.facebookAppId,
+    facebookPageUrl: config.facebookPageUrl,
+    twitterCardType: config.twitterCardType,
+    twitterAccount: config.twitterAccount,
+
     lang: req.lang,
     path: req.originalUrl,
-    dic,
+
     title: `${dic.t('Templates.Foods.FOODS')} | ${dic.t('name')}`,
     description: dic.t('Templates.Foods.description'),
-    thumbnailUrl: 'TODO',
+    keywords: ['hirayama', '平山', 'coffee', 'コーヒー', '珈琲', 'institute', '研究所'],
+    image: 'TODO',
     pageType: 'food',
 
-    heading: dic.t('Templates.Foods.FOODS'),
+    header: {
+      lang: req.lang,
+    },
+    navigation: {
+      path: req.originalUrl,
+    },
+    categoryTitle: {
+      heading: dic.t('Templates.Foods.FOODS'),
+    },
     items,
-  });
+  };
+
+  res.render('templates/Menu', { dic, props });
 }
