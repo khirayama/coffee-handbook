76e471435ea8a6c712f7f629d3f3243ac3eb722f
Kotaro Hirayama <just.do.it.my.life@gmail.com>
2018-06-21 07:52:25 +0900

    Update Goods type

--- src/handlers/goodsHandler.ts
+++ src/handlers/goodsHandler.ts
@@ -1,25 +1,57 @@
+import * as express from 'express';
+
 import { config } from 'config';
-import { IGoodsPage } from 'presentations/pages/Goods/index';
+import { IGoodCardComponent } from 'presentations/components/GoodCard';
+import { IGoodsPage } from 'presentations/pages/Goods';
 import { Good, IGood } from 'resources/Good';
 import { Dictionary } from 'utils/Dictionary';
 
-export function goodsHandler(req: any, res: any): void {
+export function goodsHandler(req: express.Request, res: express.Response): void {
   const dic: Dictionary = new Dictionary(req.lang);
   const goods: IGood[] = Good(req.lang).find();
 
-  const vars: IGoodsPage = {
-    config,
+  const props: IGoodsPage = {
+    author: dic.t('author'),
+    name: dic.t('name'),
+    baseUrl: config.url,
+    facebookAppId: config.facebookAppId,
+    facebookPageUrl: config.facebookPageUrl,
+    twitterCardType: config.twitterCardType,
+    twitterAccount: config.twitterAccount,
+
     lang: req.lang,
     path: req.originalUrl,
-    dic,
+
     title: `${dic.t('Pages.Goods.GOODS')} | ${dic.t('name')}`,
     description: dic.t('Pages.Goods.description'),
-    thumbnailUrl: 'TODO',
+    keywords: ['hirayama', '平山', 'coffee', 'コーヒー', '珈琲', 'institute', '研究所'],
+    image: 'TODO',
     pageType: 'product',
 
-    heading: dic.t('Pages.Goods.GOODS'),
-    goods,
+    header: {
+      lang: req.lang,
+    },
+    navigation: {
+      path: req.originalUrl,
+    },
+    categoryTitle: {
+      heading: dic.t('Pages.Goods.GOODS'),
+    },
+    goodList: goods.map(
+      (good: IGood): IGoodCardComponent => {
+        return {
+          url: good.url,
+          name: good.name,
+          category: good.category,
+          picture: {
+            src: good.meta.thumbnailUrl.square,
+            alt: good.meta.title,
+            lazy: true,
+          },
+        };
+      },
+    ),
   };
 
-  res.render('pages/Goods', vars);
+  res.render('pages/Goods', { dic, props });
 }
