d5bf3327608e5d006dbff5c04a35e2315ff7bae3
Kotaro Hirayama <just.do.it.my.life@gmail.com>
2018-06-21 07:52:31 +0900

    Update Good type

--- src/handlers/goodHandler.ts
+++ src/handlers/goodHandler.ts
@@ -1,9 +1,12 @@
+import * as express from 'express';
+
 import { config } from 'config';
-import { IGoodTemplate } from 'presentations/templates/Good/index';
+import { IPictureComponent } from 'presentations/components/Picture';
+import { IGoodTemplate, ISpecRowComponent } from 'presentations/templates/Good';
 import { Good, IGood } from 'resources/Good';
 import { Dictionary } from 'utils/Dictionary';
 
-export function goodHandler(req: any, res: any): void {
+export function goodHandler(req: express.Request, res: express.Response): void {
   const dic: Dictionary = new Dictionary(req.lang);
   const good: IGood = Good(req.lang)
     .where({
@@ -11,18 +14,66 @@ export function goodHandler(req: any, res: any): void {
     })
     .findOne();
 
-  const vars: IGoodTemplate = {
-    config,
+  const props: IGoodTemplate = {
+    author: dic.t('author'),
+    name: dic.t('name'),
+    baseUrl: config.url,
+    facebookAppId: config.facebookAppId,
+    facebookPageUrl: config.facebookPageUrl,
+    twitterCardType: config.twitterCardType,
+    twitterAccount: config.twitterAccount,
+
     lang: req.lang,
     path: req.originalUrl,
-    dic,
+
     title: `${good.meta.title} | ${dic.t('name')}`,
     description: good.meta.description,
-    thumbnailUrl: good.meta.thumbnailUrl.rectangle,
+    keywords: ['hirayama', '平山', 'coffee', 'コーヒー', '珈琲', 'institute', '研究所'],
+    image: good.meta.thumbnailUrl.rectangle,
     pageType: 'product',
 
-    good,
+    header: {
+      lang: req.lang,
+    },
+    navigation: {
+      path: req.originalUrl,
+    },
+    good: {
+      name: good.name,
+      category: good.category,
+      summary: good.summary,
+      content: good.content,
+      pictures: good.pictures.map(
+        (picture: { url: string; caption: string }): IPictureComponent => {
+          return {
+            src: picture.url,
+            alt: picture.caption,
+            lazy: true,
+          };
+        },
+      ),
+      specs: good.specs.map(
+        (spec: { name: string; value: string }): ISpecRowComponent => {
+          return {
+            name: spec.name,
+            value: spec.value,
+          };
+        },
+      ),
+    },
+    relatedGoods: [
+      {
+        url: 'TODO',
+        name: good.name,
+        category: good.category,
+        picture: {
+          src: good.meta.thumbnailUrl.square,
+          alt: good.name,
+          lazy: true,
+        },
+      },
+    ],
   };
 
-  res.render('templates/Good', vars);
+  res.render('templates/Good', { dic, props });
 }
