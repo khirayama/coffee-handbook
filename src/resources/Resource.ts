6527ce8f6d2fc31c777f9c6e3ed05b94f24589d7
Kotaro Hirayama <just.do.it.my.life@gmail.com>
2018-06-22 07:06:21 +0900

    Refactor Resource

--- src/resources/Resource.ts
+++ src/resources/Resource.ts
@@ -1,31 +1,5 @@
+// tslint:disable:no-any
 export type TResource<IRawResource, IResource> = (lang: string) => Resource<IRawResource, IResource>;
-
-function build(value: any, lang: string): any {
-  if (Array.isArray(value)) {
-    const res: any[] = [];
-    for (let i: number = 0; i < value.length; i += 1) {
-      const val: any = value[i];
-      res[i] = build(val, lang);
-    }
-
-    return res;
-  }
-
-  if (typeof value === 'boolean' || typeof value === 'string' || typeof value === 'number' || value === null) {
-    return value;
-  }
-
-  const result: any = {};
-  const keys: string[] = Object.keys(value);
-  for (const key of keys) {
-    if (key === lang) {
-      return build(value[key], lang);
-    }
-    result[key] = build(value[key], lang);
-  }
-
-  return result;
-}
 
 /* Usecase
 find, findOne, where, page, order
@@ -41,19 +15,19 @@ export class Resource<IRawResource, IResource> {
   constructor(resources: IRawResource[], lang: string) {
     this.resources = resources;
     this.lang = lang;
-    this.tmp = build(this.resources, this.lang);
+    this.tmp = this.build(this.resources, this.lang);
   }
 
   public find(num?: number): IResource[] {
     const tmp: IResource[] = this.tmp.slice();
-    this.tmp = build(this.resources, this.lang);
+    this.tmp = this.build(this.resources, this.lang);
 
     return tmp.slice(tmp.length - num);
   }
 
   public findOne(): IResource {
     const tmp: IResource[] = this.tmp.slice();
-    this.tmp = build(this.resources, this.lang);
+    this.tmp = this.build(this.resources, this.lang);
 
     return tmp[0];
   }
@@ -68,39 +42,70 @@ export class Resource<IRawResource, IResource> {
     return this;
   }
 
-  private include(items: any[], condition: any): any {
-    let result: any[] = items;
+  private build(value: IRawResource[], lang: string): IResource[] {
+    if (Array.isArray(value)) {
+      const res: any[] = [];
+      for (let i: number = 0; i < value.length; i += 1) {
+        const val: any = value[i];
+        res[i] = this.build(val, lang);
+      }
+
+      return res;
+    }
+
+    if (typeof value === 'boolean' || typeof value === 'string' || typeof value === 'number' || value === null) {
+      return value;
+    }
+
+    const result: any = {};
+    const keys: string[] = Object.keys(value);
+    for (const key of keys) {
+      if (key === lang) {
+        return this.build(value[key], lang);
+      }
+      result[key] = this.build(value[key], lang);
+    }
+
+    return result;
+  }
+
+  private include(items: IResource[], condition: any): IResource[] {
+    let result: IResource[] = items;
     const keys: string[] = Object.keys(condition);
     for (const key of keys) {
       const val: any = condition[key];
       if (key !== 'excepted') {
-        result = result.filter((item: any) => {
-          const target: any = item[key];
-          if (Array.isArray(target)) {
-            return this.include(target, val).length;
-          }
+        result = result.filter(
+          (item: IResource): boolean => {
+            const target: any = item[key];
+            if (Array.isArray(target)) {
+              return !!this.include(target, val).length;
+            }
 
-          return target === val;
-        });
+            return target === val;
+          },
+        );
       }
     }
 
     return result;
   }
 
-  private except(items: any[], condition: any): any {
-    let result: any[] = items;
+  private except(items: IResource[], condition: any): IResource[] {
+    let result: IResource[] = items;
     const keys: string[] = Object.keys(condition);
     for (const key of keys) {
       const val: any = condition[key];
-      result = result.filter((item: any) => {
-        const target: any = item[key];
-        if (Array.isArray(target)) {
-          return !this.except(target, val).length;
-        }
+      result = result.filter(
+        (item: IResource): boolean => {
+          const target: any = item[key];
+          if (Array.isArray(target)) {
+            return !this.except(target, val).length;
+          }
 
-        return target !== val;
-      });
+          return target !== val;
+        },
+      );
     }
 
     return result;
