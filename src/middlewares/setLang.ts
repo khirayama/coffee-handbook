783352fecf9f158300c489fb302742da4c3dc6d5
Kotaro Hirayama <just.do.it.my.life@gmail.com>
2018-06-23 06:47:12 +0900

    Set dic to req in middlewares/setLang

--- src/middlewares/setLang.ts
+++ src/middlewares/setLang.ts
@@ -1,6 +1,7 @@
 import * as express from 'express';
 
 import { config } from 'config';
+import { Dictionary } from 'utils/Dictionary';
 
 export function setLang(req: express.Request, res: express.Response, next: express.NextFunction): void {
   let lang: string = req.query.lang || req.cookies.lang || config.languages[0];
@@ -12,5 +13,6 @@ export function setLang(req: express.Request, res: express.Response, next: expre
     httpOnly: false,
   });
   req.lang = lang;
+  req.dic = new Dictionary(req.lang);
   next();
 }
