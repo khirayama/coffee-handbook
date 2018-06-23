7c8771d6f9634a1994f94ce0eb2462cf070416a2
Kotaro Hirayama <just.do.it.my.life@gmail.com>
2018-06-24 07:52:08 +0900

    Set gaCode

--- src/middlewares/setLayoutProps.ts
+++ src/middlewares/setLayoutProps.ts
@@ -1,9 +1,13 @@
 import * as express from 'express';
 
 import { config } from 'config';
+import { secret } from 'secret';
 
 export function setLayoutProps(req: express.Request, res: express.Response, next: express.NextFunction): void {
   req.layout = {
+    env: process.env.NODE_ENV || 'development',
+    gaCode: secret.gaCode,
+
     author: req.dic.t('author'),
     name: req.dic.t('name'),
     baseUrl: config.url,
