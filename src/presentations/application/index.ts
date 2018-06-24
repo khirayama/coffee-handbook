fe576f0871deade9b356e6cfa9f48fd14da77204
Kotaro Hirayama <just.do.it.my.life@gmail.com>
2018-06-24 13:12:32 +0900

    Refactor application/index.ts

--- src/presentations/application/index.ts
+++ src/presentations/application/index.ts
@@ -1,4 +1,4 @@
-import { IEventOptions, IExceptionOptions, IPageViewOptions, Tracker } from 'presentations/application/Tracker';
+import { tracker, Tracker } from 'presentations/application/tracker';
 
 declare global {
   // tslint:disable-next-line:interface-name
@@ -10,36 +10,26 @@ declare global {
       route: string;
     };
     tracker: Tracker;
-    ga(...args: (string | IPageViewOptions | IEventOptions | IExceptionOptions)[]): void;
   }
 }
 
-// Service Worker
-window.addEventListener('DOMContentLoaded', () => {
-  if ('serviceWorker' in navigator) {
-    navigator.serviceWorker.register('/service-worker.js');
-  }
-});
-
 // Tracker
 const route: string = window.options.route;
-const currentLoc: string =
+const loc: string =
   window.location.href.indexOf('lang=') === -1
     ? `${window.location.href}?lang=${window.options.lang}`
     : window.location.href;
 
-window.tracker = new Tracker({
-  code: window.options.gaCode,
-  debug: window.options.env !== 'production',
-});
-window.tracker.setPage(route);
-window.tracker.setLocation(currentLoc);
-window.tracker.sendPageView();
+tracker.setPage(route);
+tracker.setLocation(loc);
+tracker.sendPageView();
+
+// Tracking Error
 window.addEventListener(
   'error',
   (evt: ErrorEvent) => {
     let message: string = 'Error: Unknown';
-    const target: HTMLElement = <HTMLElement> evt.target;
+    const target: HTMLElement = <HTMLElement>evt.target;
     if (evt.message) {
       message = evt.message;
     } else if (target instanceof HTMLImageElement) {
@@ -49,7 +39,14 @@ window.addEventListener(
     } else if (target instanceof HTMLLinkElement) {
       message = `No Stylesheet: ${target.href}`;
     }
-    window.tracker.sendException(`${message} at ${currentLoc}`);
+    tracker.sendException(`${message} at ${loc}`);
   },
   true,
 );
+
+// Service Worker
+window.addEventListener('DOMContentLoaded', () => {
+  if ('serviceWorker' in navigator) {
+    navigator.serviceWorker.register('/service-worker.js');
+  }
+});
