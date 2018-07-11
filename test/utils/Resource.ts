bb522323eea5519fed6c4f7a1cc82f6e27325389
Kotaro Hirayama <just.do.it.my.life@gmail.com>
2018-07-12 06:36:40 +0900

    Add test cases for utils/Resource

---
+++ test/utils/Resource.ts
@@ -0,0 +1,72 @@
+import * as assert from 'power-assert';
+
+import { Resource, TResource } from 'utils/Resource';
+
+interface ISample {
+  key: string;
+  data: {
+    category: string;
+  };
+}
+
+const samples: ISample[] = [{
+  key: 'sample-1',
+  data: {
+    category: 'sample-category-1',
+  },
+}, {
+  key: 'sample-2',
+  data: {
+    category: 'sample-category-2',
+  },
+}, {
+  key: 'sample-3',
+  data: {
+    category: 'sample-category-3',
+  },
+}];
+
+// tslint:disable-next-line:variable-name
+const SampleResouce: TResource<ISample, ISample> = (lang: string): Resource<ISample, ISample> => {
+  return new Resource(samples, lang);
+};
+
+describe('Resource', () => {
+  describe('where', () => {
+    it('normal', () => {
+      const sample2: ISample = SampleResouce('').where({
+        key: 'sample-2',
+      }).findOne();
+      assert.deepEqual(sample2, {
+        key: 'sample-2',
+        data: {
+          category: 'sample-category-2',
+        },
+      });
+    });
+
+    it('nested condition', () => {
+      const sample2: ISample = SampleResouce('').where({
+        data: {
+          category: 'sample-category-2',
+        }
+      }).findOne();
+      assert.deepEqual(sample2, {
+        key: 'sample-2',
+        data: {
+          category: 'sample-category-2',
+        },
+      });
+    });
+
+    it('nested no matched condition', () => {
+      const sample2: ISample = SampleResouce('').where({
+        key: 'sample2',
+        data: {
+          category: 'sample-category-1',
+        }
+      }).findOne();
+      assert.deepEqual(sample2, null);
+    });
+  });
+});
