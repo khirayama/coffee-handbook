7d8efa2f8ff3791766b7327281254ac6a42c5471
Kotaro Hirayama <just.do.it.my.life@gmail.com>
2018-07-12 08:18:06 +0900

    Update madeleineRecipe

--- src/data/recipes/madeleineRecipe.ts
+++ src/data/recipes/madeleineRecipe.ts
@@ -16,8 +16,8 @@ export const madeleineRecipe: IRawPost<IRawRecipe> = {
       en: 'MADELEINE',
     },
     description: {
-      ja: 'マドレーヌ desc',
-      en: 'MADELEINE desc',
+      ja: 'フランス発祥の焼き菓子、マドレーヌのレシピ。',
+      en: 'A recipe of madeleine that traditional small cake from the Lorraine region in northeastern France.',
     },
     thumbnailUrl: {
       square: {
@@ -44,42 +44,84 @@ export const madeleineRecipe: IRawPost<IRawRecipe> = {
           en: 'Butter',
         },
         quantity: {
-          ja: '20g',
-          en: '20g',
+          ja: '100g',
+          en: '100g',
         },
         note: {
-          ja: '中細挽き',
-          en: 'Middle casdcadscasdcasdcas',
+          ja: 'レンジ(500W 30~45秒ほど)で柔らかくする。',
+          en: 'Get it soft using microwave.',
+        },
+      },
+      {
+        name: {
+          ja: 'ホットケーキミックス',
+          en: 'Hotcake mix',
+        },
+        quantity: {
+          ja: '100g',
+          en: '100g',
+        },
+      },
+      {
+        name: {
+          ja: '砂糖',
+          en: 'Sugers',
+        },
+        quantity: {
+          ja: '80g',
+          en: '80g',
+        },
+      },
+      {
+        name: {
+          ja: '卵',
+          en: 'Eggs',
+        },
+        quantity: {
+          ja: '2個',
+          en: '2',
         },
       },
     ],
     steps: [
       {
         summary: {
-          ja: '水をあれあれ',
-          en: 'Water Water',
+          ja: '生地を作る。',
+          en: 'Make batters.',
         },
         description: {
-          ja: 'いろいろ細かく',
-          en: 'Do something',
+          ja: '柔らかくしたバター、ホットケーキミックス、砂糖、卵を全部混ぜ合わせる。',
+          en: 'Mixs butter, hotcake mix, sugars and eggs.',
         },
         note: {
-          ja: 'ほげほげ',
-          en: 'hogehoge',
+          ja: 'バターは分離前に早めに混ぜる。',
+          en: "It's better to mix butter before separation.",
         },
       },
       {
         summary: {
-          ja: '水をあれあれ',
-          en: 'Water Water',
+          ja: '焼き上げる。',
+          en: 'Bakes batters.',
         },
         description: {
-          ja: 'いろいろ細かく',
-          en: 'Do something',
+          ja: 'マドレーヌ型に生地を流し込み、180&#8451; 20分で焼き上げる。',
+          en: 'Puts batters into a mold. And bakes with 180&#8451; for 20mins.',
         },
-        note: {
-          ja: 'ほげほげ',
-          en: 'hogehoge',
+      },
+      {
+        summary: {
+          ja: '出来上がり！',
+          en: "It's ready to eat!",
+        },
+        image: {
+          src: {
+            ja: '/images/image_1@square.jpg',
+            en: '/images/image_1@square.jpg',
+          },
+          caption: {
+            ja: 'マドレーヌ 出来上がり！',
+            en: 'Madeleine is ready to eat!',
+          },
         },
       },
     ],
