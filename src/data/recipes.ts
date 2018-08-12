682c3f85b96b02135987faedac4652c82da8bc2c
Kotaro Hirayama <just.do.it.my.life@gmail.com>
2018-08-12 21:52:46 +0900

    Define COLD BREW

--- src/data/recipes.ts
+++ src/data/recipes.ts
@@ -1,10 +1,36 @@
 // Beverages
 import { coffeeHotRecipe } from 'data/recipes/coffeeHotRecipe';
 import { coffeeIcedRecipe } from 'data/recipes/coffeeIcedRecipe';
+import { coldBrewRecipe } from 'data/recipes/coldBrewRecipe';
 // Foods
 import { madeleineRecipe } from 'data/recipes/madeleineRecipe';
 
 import { IRawPost } from 'data/posts';
+
+interface IRawRecipeStep {
+  summary: {
+    ja: string;
+    en: string;
+  };
+  description?: {
+    ja: string;
+    en: string;
+  };
+  image?: {
+    src: {
+      ja: string;
+      en: string;
+    };
+    caption: {
+      ja: string;
+      en: string;
+    };
+  };
+  note?: {
+    ja: string;
+    en: string;
+  };
+}
 
 export interface IRawRecipe {
   category: {
@@ -33,30 +59,7 @@ export interface IRawRecipe {
       en: string;
     };
   }[];
-  steps: {
-    summary: {
-      ja: string;
-      en: string;
-    };
-    description?: {
-      ja: string;
-      en: string;
-    };
-    image?: {
-      src: {
-        ja: string;
-        en: string;
-      };
-      caption: {
-        ja: string;
-        en: string;
-      };
-    };
-    note?: {
-      ja: string;
-      en: string;
-    };
-  }[];
+  steps: IRawRecipeStep[];
 }
 
-export const recipes: IRawPost<IRawRecipe>[] = [coffeeHotRecipe, coffeeIcedRecipe, madeleineRecipe];
+export const recipes: IRawPost<IRawRecipe>[] = [coffeeHotRecipe, coffeeIcedRecipe, coldBrewRecipe, madeleineRecipe];
---
+++ src/data/recipes/coldBrewRecipe.ts
@@ -0,0 +1,101 @@
+import { IRawPost } from 'data/posts';
+import { IRawRecipe } from 'data/recipes';
+import { dictionary } from 'dictionary';
+import { Dictionary } from 'utils/Dictionary';
+
+const dic: Dictionary = new Dictionary(null, dictionary);
+
+export const coldBrewRecipe: IRawPost<IRawRecipe> = {
+  key: 'cold-brew-iced',
+  meta: {
+    createdAt: '2018-05-26 12:00',
+    publishedAt: '2018-05-27 16:00',
+    url: '/beverages/cold-brew/iced',
+    title: {
+      ja: 'コールドブリュー',
+      en: 'COLD BREW',
+    },
+    description: {
+      ja: 'コールドブリュー desc',
+      en: 'COLD BREW desc',
+    },
+    thumbnailUrl: {
+      square: {
+        ja: '/images/image_1@square.jpg',
+        en: '/images/image_1@square.jpg',
+      },
+      rectangle: {
+        ja: '/images/image_1@rectangle.jpg',
+        en: '/images/image_1@rectangle.jpg',
+      },
+    },
+  },
+  data: {
+    category: dic.v('meta.recipe.category.BEVERAGES'),
+    recipeType: dic.v('meta.recipe.recipeType.ICED'),
+    name: {
+      ja: 'コールドブリュー',
+      en: 'COLD BREW',
+    },
+    ingredients: [
+      {
+        name: {
+          ja: 'コーヒー豆',
+          en: 'Coffee Beans',
+        },
+        quantity: {
+          ja: '20g',
+          en: '20g',
+        },
+        note: {
+          ja: '中細挽き',
+          en: 'Middle casdcadscasdcasdcas',
+        },
+      },
+      {
+        name: {
+          ja: '水',
+          en: 'Water',
+        },
+        quantity: {
+          ja: '200ml',
+          en: '200ml',
+        },
+        note: {
+          ja: '94℃程度',
+          en: 'Around 94℃(201℉)',
+        },
+      },
+    ],
+    steps: [
+      {
+        summary: {
+          ja: '水をあれあれ',
+          en: 'Water Water',
+        },
+        description: {
+          ja: 'いろいろ細かく',
+          en: 'Do something',
+        },
+        note: {
+          ja: 'ほげほげ',
+          en: 'hogehoge',
+        },
+      },
+      {
+        summary: {
+          ja: '水をあれあれ',
+          en: 'Water Water',
+        },
+        description: {
+          ja: 'いろいろ細かく',
+          en: 'Do something',
+        },
+        note: {
+          ja: 'ほげほげ',
+          en: 'hogehoge',
+        },
+      },
+    ],
+  },
+};
