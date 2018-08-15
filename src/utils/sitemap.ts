6578810146bb04fbdde2d61997789e8933d7ba04
Kotaro Hirayama <just.do.it.my.life@gmail.com>
2018-08-16 08:19:23 +0900

    Format utils/sitemap

--- src/utils/sitemap.ts
+++ src/utils/sitemap.ts
@@ -21,178 +21,205 @@ interface IPage {
   pages?: IPage[];
 }
 
-const coffeeHandbookPages: IPage[] = [{
-  name: {
-    en: 'COFFEE HANDBOOK',
-    ja: '珈琲手帖',
+const coffeeHandbookPages: IPage[] = [
+  {
+    name: {
+      en: 'COFFEE HANDBOOK',
+      ja: '珈琲手帖',
+    },
+    url: `${config.url}`,
+    freq: 'daily',
+    priority: 1,
   },
-  url: `${config.url}`,
-  freq: 'daily',
-  priority: 1,
-}, {
-  name: {
-    en: 'BEVERAGES',
-    ja: 'ビバレッジ',
+  {
+    name: {
+      en: 'BEVERAGES',
+      ja: 'ビバレッジ',
+    },
+    url: `${config.url}/beverages`,
+    freq: 'daily',
+    priority: 0.8,
+    pages: [
+      {
+        name: {
+          en: 'COFFEE(HOT)',
+          ja: 'コーヒー(ホット)',
+        },
+        url: `${config.url}/beverages/coffee/hot`,
+        freq: 'daily',
+        priority: 0.5,
+      },
+      {
+        name: {
+          en: 'COFFEE(ICED)',
+          ja: 'コーヒー(アイス)',
+        },
+        url: `${config.url}/beverages/coffee/iced`,
+        freq: 'daily',
+        priority: 0.5,
+      },
+      {
+        name: {
+          en: 'COLD BREW',
+          ja: 'コールドブリュー',
+        },
+        url: `${config.url}/beverages/cold-brew/iced`,
+        freq: 'daily',
+        priority: 0.5,
+      },
+      {
+        name: {
+          en: 'LATTE(HOT)',
+          ja: 'ラテ(ホット)',
+        },
+        url: `${config.url}/beverages/latte/hot`,
+        freq: 'daily',
+        priority: 0.5,
+      },
+      {
+        name: {
+          en: 'LATTE(ICED)',
+          ja: 'ラテ(アイス)',
+        },
+        url: `${config.url}/beverages/latte/iced`,
+        freq: 'daily',
+        priority: 0.5,
+      },
+      {
+        name: {
+          en: 'AMERICANO(HOT)',
+          ja: 'アメリカーノ(ホット)',
+        },
+        url: `${config.url}/beverages/americano/hot`,
+        freq: 'daily',
+        priority: 0.5,
+      },
+      {
+        name: {
+          en: 'AMERICANO(ICED)',
+          ja: 'アメリカーノ(アイス)',
+        },
+        url: `${config.url}/beverages/americano/iced`,
+        freq: 'daily',
+        priority: 0.5,
+      },
+      {
+        name: {
+          en: 'MOCHA(HOT)',
+          ja: 'モカ(ホット)',
+        },
+        url: `${config.url}/beverages/mocha/hot`,
+        freq: 'daily',
+        priority: 0.5,
+      },
+      {
+        name: {
+          en: 'MOCHA(ICED)',
+          ja: 'モカ(アイス)',
+        },
+        url: `${config.url}/beverages/mocha/iced`,
+        freq: 'daily',
+        priority: 0.5,
+      },
+      {
+        name: {
+          en: 'VANILLA LATTE(HOT)',
+          ja: 'バニララテ(ホット)',
+        },
+        url: `${config.url}/beverages/vanilla-latte/hot`,
+        freq: 'daily',
+        priority: 0.5,
+      },
+      {
+        name: {
+          en: 'VANILLA LATTE(ICED)',
+          ja: 'バニララテ(アイス)',
+        },
+        url: `${config.url}/beverages/vanilla-latte/iced`,
+        freq: 'daily',
+        priority: 0.5,
+      },
+      {
+        name: {
+          en: 'COCOA(HOT)',
+          ja: 'ココア(ホット)',
+        },
+        url: `${config.url}/beverages/cocoa/hot`,
+        freq: 'daily',
+        priority: 0.5,
+      },
+      {
+        name: {
+          en: 'COCOA(ICED)',
+          ja: 'ココア(アイス)',
+        },
+        url: `${config.url}/beverages/cocoa/iced`,
+        freq: 'daily',
+        priority: 0.5,
+      },
+      {
+        name: {
+          en: 'ESPRESSO',
+          ja: 'エスプレッソ',
+        },
+        url: `${config.url}/beverages/espresso/hot`,
+        freq: 'daily',
+        priority: 0.5,
+      },
+    ],
   },
-  url: `${config.url}/beverages`,
-  freq: 'daily',
-  priority: 0.8,
-  pages: [{
+  {
     name: {
-      en: 'COFFEE(HOT)',
-      ja: 'コーヒー(ホット)',
+      en: 'FOODS',
+      ja: 'フード',
     },
-    url: `${config.url}/beverages/coffee/hot`,
+    url: `${config.url}/foods`,
     freq: 'daily',
-    priority: 0.5,
-  }, {
-    name: {
-      en: 'COFFEE(ICED)',
-      ja: 'コーヒー(アイス)',
-    },
-    url: `${config.url}/beverages/coffee/iced`,
-    freq: 'daily',
-    priority: 0.5,
-  }, {
-    name: {
-      en: 'COLD BREW',
-      ja: 'コールドブリュー',
-    },
-    url: `${config.url}/beverages/cold-brew/iced`,
-    freq: 'daily',
-    priority: 0.5,
-  }, {
-    name: {
-      en: 'LATTE(HOT)',
-      ja: 'ラテ(ホット)',
-    },
-    url: `${config.url}/beverages/latte/hot`,
-    freq: 'daily',
-    priority: 0.5,
-  }, {
-    name: {
-      en: 'LATTE(ICED)',
-      ja: 'ラテ(アイス)',
-    },
-    url: `${config.url}/beverages/latte/iced`,
-    freq: 'daily',
-    priority: 0.5,
-  }, {
-    name: {
-      en: 'AMERICANO(HOT)',
-      ja: 'アメリカーノ(ホット)',
-    },
-    url: `${config.url}/beverages/americano/hot`,
-    freq: 'daily',
-    priority: 0.5,
-  }, {
-    name: {
-      en: 'AMERICANO(ICED)',
-      ja: 'アメリカーノ(アイス)',
-    },
-    url: `${config.url}/beverages/americano/iced`,
-    freq: 'daily',
-    priority: 0.5,
-  }, {
-    name: {
-      en: 'MOCHA(HOT)',
-      ja: 'モカ(ホット)',
-    },
-    url: `${config.url}/beverages/mocha/hot`,
-    freq: 'daily',
-    priority: 0.5,
-  }, {
-    name: {
-      en: 'MOCHA(ICED)',
-      ja: 'モカ(アイス)',
-    },
-    url: `${config.url}/beverages/mocha/iced`,
-    freq: 'daily',
-    priority: 0.5,
-  }, {
-    name: {
-      en: 'VANILLA LATTE(HOT)',
-      ja: 'バニララテ(ホット)',
-    },
-    url: `${config.url}/beverages/vanilla-latte/hot`,
-    freq: 'daily',
-    priority: 0.5,
-  }, {
-    name: {
-      en: 'VANILLA LATTE(ICED)',
-      ja: 'バニララテ(アイス)',
-    },
-    url: `${config.url}/beverages/vanilla-latte/iced`,
-    freq: 'daily',
-    priority: 0.5,
-  }, {
-    name: {
-      en: 'COCOA(HOT)',
-      ja: 'ココア(ホット)',
-    },
-    url: `${config.url}/beverages/cocoa/hot`,
-    freq: 'daily',
-    priority: 0.5,
-  }, {
-    name: {
-      en: 'COCOA(ICED)',
-      ja: 'ココア(アイス)',
-    },
-    url: `${config.url}/beverages/cocoa/iced`,
-    freq: 'daily',
-    priority: 0.5,
-  }, {
-    name: {
-      en: 'ESPRESSO',
-      ja: 'エスプレッソ',
-    },
-    url: `${config.url}/beverages/espresso/hot`,
-    freq: 'daily',
-    priority: 0.5,
-  }],
-}, {
-  name: {
-    en: 'FOODS',
-    ja: 'フード',
+    priority: 0.8,
+    pages: [
+      {
+        name: {
+          en: 'MADELEINE',
+          ja: 'マドレーヌ',
+        },
+        url: `${config.url}/foods/madeleine`,
+        freq: 'daily',
+        priority: 0.5,
+      },
+    ],
   },
-  url: `${config.url}/foods`,
-  freq: 'daily',
-  priority: 0.8,
-  pages: [{
+  {
     name: {
-      en: 'MADELEINE',
-      ja: 'マドレーヌ',
+      en: 'GOODS',
+      ja: 'グッズ',
     },
-    url: `${config.url}/foods/madeleine`,
+    url: `${config.url}/goods`,
     freq: 'daily',
-    priority: 0.5,
-  }],
-}, {
-  name: {
-    en: 'GOODS',
-    ja: 'グッズ',
+    priority: 0.8,
+    pages: [],
   },
-  url: `${config.url}/goods`,
-  freq: 'daily',
-  priority: 0.8,
-  pages: [],
-}, {
-  name: {
-    en: 'ABOUT US',
-    ja: '私たちについて',
+  {
+    name: {
+      en: 'ABOUT US',
+      ja: '私たちについて',
+    },
+    url: `${config.url}/about-us`,
+    freq: 'daily',
+    priority: 0.3,
+    pages: [],
   },
-  url: `${config.url}/about-us`,
-  freq: 'daily',
-  priority: 0.3,
-  pages: [],
-}];
+];
 
 function xmlUrlTemplate(page: IPage): string {
-  return `<url><loc>${page.url}</loc>${page.lastmod ? `<lastmod>${page.lastmod}</lastmod>` : ''}${page.freq ? `<changefreq>${page.freq}</changefreq>` : ''}${page.priority ? `<priority>${page.priority}</priority>` : ''}</url>`;
+  return `<url><loc>${page.url}</loc>${page.lastmod ? `<lastmod>${page.lastmod}</lastmod>` : ''}${
+    page.freq ? `<changefreq>${page.freq}</changefreq>` : ''
+  }${page.priority ? `<priority>${page.priority}</priority>` : ''}</url>`;
 }
 
 function xmlSitemapTemplate(urlTemplates: string[]): string {
-  return `<?xml version="1.0" encoding="UTF-8"?><urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">${urlTemplates.join('')}</urlset>`;
+  return `<?xml version="1.0" encoding="UTF-8"?><urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">${urlTemplates.join(
+    '',
+  )}</urlset>`;
 }
 
 function buildUrlTemplates(pages: IPage[]): string[] {
