4496ab692fff53ab7a91448d1036074ba2d6496b
Kotaro Hirayama <just.do.it.my.life@gmail.com>
2018-08-22 09:50:07 +0900

    Add vanilla latte recipes

---
+++ src/data/recipes/vanillaLatteIcedRecipe.ts
@@ -0,0 +1,101 @@
+import { IRawPost } from 'data/posts';
+import { IRawRecipe } from 'data/recipes';
+import { dictionary } from 'dictionary';
+import { Dictionary } from 'utils/Dictionary';
+
+const dic: Dictionary = new Dictionary(null, dictionary);
+
+export const vanillaLatteIcedRecipe: IRawPost<IRawRecipe> = {
+  key: 'vanilla-latte-iced',
+  meta: {
+    createdAt: '2018-05-26 12:00',
+    publishedAt: '2018-05-27 16:00',
+    url: '/beverages/vanilla-latte/iced',
+    title: {
+      ja: 'バニララテ',
+      en: 'VANILLA LATTE',
+    },
+    description: {
+      ja: 'バニララテ desc',
+      en: 'VANILLA LATTE desc',
+    },
+    thumbnailUrl: {
+      square: {
+        ja: '/images/image_1@square.jpg',
+        en: '/images/image_1@square.jpg',
+      },
+      rectangle: {
+        ja: '/images/image_1@rectangle.jpg',
+        en: '/images/image_1@rectangle.jpg',
+      },
+    },
+  },
+  data: {
+    category: dic.v('meta.recipe.category.BEVERAGES'),
+    recipeType: dic.v('meta.recipe.recipeType.ICED'),
+    name: {
+      ja: 'バニララテ',
+      en: 'VANILLA LATTE',
+    },
+    ingredients: [
+      {
+        name: {
+          ja: 'コーヒー豆',
+          en: 'Coffee Beans',
+        },
+        quantity: {
+          ja: '20g',
+          en: '20g',
+        },
+        note: {
+          ja: '中細挽き',
+          en: 'Middle casdcadscasdcasdcas',
+        },
+      },
+      {
+        name: {
+          ja: '水',
+          en: 'Water',
+        },
+        quantity: {
+          ja: '200ml',
+          en: '200ml',
+        },
+        note: {
+          ja: '94℃程度',
+          en: 'Around 94℃(201℉)',
+        },
+      },
+    ],
+    steps: [
+      {
+        summary: {
+          ja: '水をあれあれ',
+          en: 'Water Water',
+        },
+        description: {
+          ja: 'いろいろ細かく',
+          en: 'Do something',
+        },
+        note: {
+          ja: 'ほげほげ',
+          en: 'hogehoge',
+        },
+      },
+      {
+        summary: {
+          ja: '水をあれあれ',
+          en: 'Water Water',
+        },
+        description: {
+          ja: 'いろいろ細かく',
+          en: 'Do something',
+        },
+        note: {
+          ja: 'ほげほげ',
+          en: 'hogehoge',
+        },
+      },
+    ],
+  },
+};
