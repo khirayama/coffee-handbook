ccd492c4d09ca64f463c31ee5b47c0bf27f3edda
Kotaro Hirayama <just.do.it.my.life@gmail.com>
2018-08-23 09:15:59 +0900

    Update recipes

--- src/data/recipes.ts
+++ src/data/recipes.ts
@@ -1,8 +1,34 @@
-// Beverages
+// Beverages - Coffee
 import { coffeeHotRecipe } from 'data/recipes/coffeeHotRecipe';
 import { coffeeIcedRecipe } from 'data/recipes/coffeeIcedRecipe';
-import { coldBrewRecipe } from 'data/recipes/coldBrewRecipe';
-// Foods
+
+// Beverages - Cold Brew
+import { coldBrewIcedRecipe } from 'data/recipes/coldBrewIcedRecipe';
+
+// Beverages - Latte
+import { latteHotRecipe } from 'data/recipes/latteHotRecipe';
+import { latteIcedRecipe } from 'data/recipes/latteIcedRecipe';
+
+// Beverages - Americano
+import { americanoHotRecipe } from 'data/recipes/americanoHotRecipe';
+import { americanoIcedRecipe } from 'data/recipes/americanoIcedRecipe';
+
+// Beverages - Mocha
+import { mochaHotRecipe } from 'data/recipes/mochaHotRecipe';
+import { mochaIcedRecipe } from 'data/recipes/mochaIcedRecipe';
+
+// Beverages - Vanilla Latte
+import { vanillaLatteHotRecipe } from 'data/recipes/vanillaLatteHotRecipe';
+import { vanillaLatteIcedRecipe } from 'data/recipes/vanillaLatteIcedRecipe';
+
+// Beverages - Cocoa
+import { cocoaHotRecipe } from 'data/recipes/cocoaHotRecipe';
+import { cocoaIcedRecipe } from 'data/recipes/cocoaIcedRecipe';
+
+// Beverages - Espresson
+import { espressoHotRecipe } from 'data/recipes/espressoHotRecipe';
+
+// Foods - Madeleine
 import { madeleineRecipe } from 'data/recipes/madeleineRecipe';
 
 import { IRawPost } from 'data/posts';
@@ -62,4 +88,22 @@ export interface IRawRecipe {
   steps: IRawRecipeStep[];
 }
 
-export const recipes: IRawPost<IRawRecipe>[] = [coffeeHotRecipe, coffeeIcedRecipe, coldBrewRecipe, madeleineRecipe];
+export const recipes: IRawPost<IRawRecipe>[] = [
+  // Beverages
+  coffeeHotRecipe,
+  coffeeIcedRecipe,
+  coldBrewIcedRecipe,
+  latteHotRecipe,
+  latteIcedRecipe,
+  americanoHotRecipe,
+  americanoIcedRecipe,
+  mochaHotRecipe,
+  mochaIcedRecipe,
+  vanillaLatteHotRecipe,
+  vanillaLatteIcedRecipe,
+  cocoaHotRecipe,
+  cocoaIcedRecipe,
+  espressoHotRecipe,
+  // Foods
+  madeleineRecipe,
+];
