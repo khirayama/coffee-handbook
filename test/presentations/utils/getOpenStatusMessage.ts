e17a5fe461c21bc36eb10d8f3674d932ab2f1428
Kotaro Hirayama <just.do.it.my.life@gmail.com>
2018-08-31 08:17:31 +0900

    Change return value from getOpenStatusMessage

--- test/presentations/utils/getOpenStatusMessage.ts
+++ test/presentations/utils/getOpenStatusMessage.ts
@@ -15,17 +15,74 @@ const sampleHours: string[][][] = [
 describe('getOpenStatusMessage', () => {
   it('run', () => {
     // 2018-8-28 Thu(2)
-    assert.equal(getOpenStatusMessage(new Date('2018-8-28 10:29'), sampleHours), '閉店 開店は 2 11:00');
-    assert.equal(getOpenStatusMessage(new Date('2018-8-28 10:30'), sampleHours), 'まもなく開店');
-    assert.equal(getOpenStatusMessage(new Date('2018-8-28 11:00'), sampleHours), '開店');
-    assert.equal(getOpenStatusMessage(new Date('2018-8-28 15:59'), sampleHours), 'まもなく閉店');
-    assert.equal(getOpenStatusMessage(new Date('2018-8-28 16:00'), sampleHours), '閉店 開店は 2 17:00');
-    assert.equal(getOpenStatusMessage(new Date('2018-8-28 16:01'), sampleHours), '閉店 開店は 2 17:00');
-    assert.equal(getOpenStatusMessage(new Date('2018-8-28 16:30'), sampleHours), 'まもなく開店');
-    assert.equal(getOpenStatusMessage(new Date('2018-8-28 17:00'), sampleHours), '開店');
-    assert.equal(getOpenStatusMessage(new Date('2018-8-28 21:29'), sampleHours), '開店');
-    assert.equal(getOpenStatusMessage(new Date('2018-8-28 21:30'), sampleHours), 'まもなく閉店');
-    assert.equal(getOpenStatusMessage(new Date('2018-8-28 22:00'), sampleHours), '閉店 開店は 4 10:00');
-    assert.equal(getOpenStatusMessage(new Date('2018-8-28 22:01'), sampleHours), '閉店 開店は 4 10:00');
+    assert.deepEqual(getOpenStatusMessage(new Date('2018-8-28 10:29'), sampleHours), {
+      openStatus: 0,
+      nextOpen: {
+        day: 2,
+        time: '11:00',
+      },
+    });
+    assert.deepEqual(getOpenStatusMessage(new Date('2018-8-28 10:30'), sampleHours), {
+      openStatus: 1,
+      nextOpen: {
+        day: 2,
+        time: '11:00',
+      },
+    });
+    assert.deepEqual(getOpenStatusMessage(new Date('2018-8-28 11:00'), sampleHours), {
+      openStatus: 2,
+      nextOpen: null,
+    });
+    assert.deepEqual(getOpenStatusMessage(new Date('2018-8-28 15:59'), sampleHours), {
+      openStatus: 3,
+      nextOpen: null,
+    });
+    assert.deepEqual(getOpenStatusMessage(new Date('2018-8-28 16:00'), sampleHours), {
+      openStatus: 0,
+      nextOpen: {
+        day: 2,
+        time: '17:00',
+      },
+    });
+    assert.deepEqual(getOpenStatusMessage(new Date('2018-8-28 16:01'), sampleHours), {
+      openStatus: 0,
+      nextOpen: {
+        day: 2,
+        time: '17:00',
+      },
+    });
+    assert.deepEqual(getOpenStatusMessage(new Date('2018-8-28 16:30'), sampleHours), {
+      openStatus: 1,
+      nextOpen: {
+        day: 2,
+        time: '17:00',
+      },
+    });
+    assert.deepEqual(getOpenStatusMessage(new Date('2018-8-28 17:00'), sampleHours), {
+      openStatus: 2,
+      nextOpen: null,
+    });
+    assert.deepEqual(getOpenStatusMessage(new Date('2018-8-28 21:29'), sampleHours), {
+      openStatus: 2,
+      nextOpen: null,
+    });
+    assert.deepEqual(getOpenStatusMessage(new Date('2018-8-28 21:30'), sampleHours), {
+      openStatus: 3,
+      nextOpen: null,
+    });
+    assert.deepEqual(getOpenStatusMessage(new Date('2018-8-28 22:00'), sampleHours), {
+      openStatus: 0,
+      nextOpen: {
+        day: 4,
+        time: '10:00',
+      },
+    });
+    assert.deepEqual(getOpenStatusMessage(new Date('2018-8-28 22:01'), sampleHours), {
+      openStatus: 0,
+      nextOpen: {
+        day: 4,
+        time: '10:00',
+      },
+    });
   });
 });
