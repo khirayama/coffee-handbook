7dcda78932d11b64e7221d03f13d8f4181d9ac99
Kotaro Hirayama <just.do.it.my.life@gmail.com>
2018-09-05 07:39:36 +0900

    Refactor Maps page

--- src/presentations/pages/Maps/index.ts
+++ src/presentations/pages/Maps/index.ts
@@ -1,10 +1,14 @@
+import * as queryString from 'query-string';
+
 import { ILayout } from 'presentations/application/Layout';
-import { IHeaderComponent } from 'presentations/components/Header';
 import { Map } from 'presentations/components/Map';
-import { INavigationComponent } from 'presentations/components/Navigation';
+import { StoreCard } from 'presentations/components/StoreCard';
 import { StoreMarker } from 'presentations/components/StoreMarker';
+import { View } from 'presentations/components/View';
+import { getOpenStatus, IOpenStatus } from 'presentations/utils/getOpenStatus';
 import { logger } from 'presentations/utils/logger';
 import { IStore, Store } from 'resources/Store';
+import { IStoreResponse, storeService } from 'services/storeService';
 
 declare global {
   // tslint:disable-next-line:interface-name
@@ -18,44 +22,87 @@ declare global {
   }
 }
 
-export interface IMapsPage extends ILayout {
-  header: IHeaderComponent;
-  navigation: INavigationComponent;
-}
-
-class ModalView {
-  private el: HTMLElement;
-
-  constructor(el: HTMLElement) {
-    this.el = el;
-  }
-
+class Modal extends View {
   public open(): void {
-    this.el.classList.add('Maps--Content--Modal__Open');
+    this.$el.addClass('Modal__Open');
   }
 
   public close(): void {
-    this.el.classList.remove('Maps--Content--Modal__Open');
+    this.$el.removeClass('Modal__Open');
   }
+}
+
+export interface IMapsPage extends ILayout {
+  store: IStore;
+  currentDay: number;
+  openStatus: IOpenStatus;
+  nextStatusMessage: string;
 }
 
 window.addEventListener('DOMContentLoaded', () => {
   logger.log(`Start app at ${new Date().toString()}.`);
+  let storeCard: StoreCard;
 
-  const stores: IStore[] = Store(window.options.lang).find();
+  const lang: string = window.options.lang;
 
-  const modalElement: HTMLElement = window.document.querySelector('.Maps--Content--Modal');
-  const modal: ModalView = new ModalView(modalElement);
+  const stores: IStore[] = Store(lang).find();
+
+  const modalElement: HTMLElement = window.document.querySelector('.Modal');
+  const modal: Modal = new Modal(modalElement);
 
   const mapElement: HTMLElement = window.document.querySelector('.Map');
   const map: Map = new Map(mapElement, {
-    onClick: (): void => modal.close(),
+    onClick: (): void => {
+      // tslint:disable-line:no-suspicious-comment TODO: window.searchのkeyを消す
+      window.history.pushState(null, 'Maps', `${window.location.pathname}`);
+      if (storeCard) {
+        storeCard.hideHours();
+      }
+      modal.close();
+    },
   });
+
+  const query: { key?: string } = queryString.parse(window.location.search);
+  if (query.key) {
+    storeService.find(query.key).then((res: IStoreResponse) => {
+      const storeCardElement: HTMLElement = modalElement.querySelector('.StoreCard');
+      storeCard = new StoreCard(storeCardElement);
+
+      map.setView({ lat: null, lng: null }, 0);
+      map.setView(
+        {
+          lat: res.lat,
+          lng: res.lng,
+        },
+        15,
+        {
+          animate: false,
+        },
+      );
+      const mapHeight: number = mapElement.clientHeight;
+      const modalHeight: number = modalElement.clientHeight;
+      const diff: number = (mapHeight - modalHeight) / 2 + modalHeight - mapHeight / 2;
+      map.panBy(0, diff, {
+        animate: false,
+        duration: 0,
+      });
+    });
+  }
+
   stores.forEach((store: IStore) => {
-    new StoreMarker(null, {
+    const storeMarker: StoreMarker = new StoreMarker(null, {
       map,
       store,
-      onClick: (): void => modal.open(),
+      onClick: (): void => {
+        // tslint:disable-line:no-suspicious-comment TODO: window.searchのkeyを足す
+        window.history.pushState(null, store.name, `${window.location.pathname}?key=${store.key}`);
+        storeService.find(store.key).then((res: IStoreResponse) => {
+          modalElement.innerHTML = res.html;
+          const storeCardElement: HTMLElement = modalElement.querySelector('.StoreCard');
+          storeCard = new StoreCard(storeCardElement);
+          modal.open();
+        });
+      },
     });
   });
 });
