f4205496678b18b1bba42d0a98f1f543b58cd214
Kotaro Hirayama <just.do.it.my.life@gmail.com>
2018-09-05 07:38:04 +0900

    Refactor StoreMarker component

--- src/presentations/components/StoreMarker.ts
+++ src/presentations/components/StoreMarker.ts
@@ -19,22 +19,6 @@ declare global {
   }
 }
 
-function nextStatus(openStatus: IOpenStatus, dic: Dictionary): string {
-  const now: Date = new Date();
-  const currentDay: number = now.getDay();
-  if (openStatus.openAt) {
-    if (currentDay === openStatus.openAt.day) {
-      return dic.t(`Pages.Maps.openAt`, openStatus.openAt.time);
-    } else {
-      return `${dic.t(`Pages.Maps.openAt`, openStatus.openAt.time)} ${dic.t(
-        `Pages.Maps.day.${openStatus.openAt.day}`,
-      )}`;
-    }
-  } else if (openStatus.closeAt) {
-    return dic.t(`Pages.Maps.closeAt`, openStatus.closeAt.time);
-  }
-}
-
 export class StoreMarker extends View {
   public props: {
     map: Map;
@@ -58,83 +42,6 @@ export class StoreMarker extends View {
   }
 
   public setEventListeners(): void {
-    this.marker.on('click', () => {
-      const store: IStore = this.props.store;
-
-      const openStatus: IOpenStatus = getOpenStatus(new Date(), store.hours);
-      const now: Date = new Date();
-      const currentDay: number = now.getDay();
-
-      const el: HTMLElement = window.document.querySelector('.Maps--Content--Modal');
-      el.innerHTML = `
-        <div class="Modal">
-          <div class="Modal--Content">
-            <h2 class="Modal--Name">${store.name}</h2>
-            <div class="Modal--Address">${store.address}</div>
-            <div class="Modal--OpenStatus">
-              <span class="Modal--OpenStatus--Status">${this.dic.t(`Pages.Maps.openStatus.${openStatus.status}`)}</span>
-              <span class="Modal--OpenStatus--NextStatus">${nextStatus(openStatus, this.dic)}</span>
-            </div>
-            <ul class="Modal--Hours">${store.hours
-              .map((hours: string[][], i: number) => {
-                if (hours.length) {
-                  return hours.map((hour: string[]) => {
-                    return `
-                      <li class="Modal--Hours--Item ${currentDay === i ? 'Modal--Hours--Item__Active' : ''}">
-                        <span class="Modal--Hours--Item--Day">${this.dic.t(`Pages.Maps.day.${i}`)}</span>
-                        <span class="Modal--Hours--Item--Time">${hour.join(' - ')}</span>
-                      </li>
-                    `;
-                  });
-                } else {
-                  return `
-                    <li class="Modal--Hours--Item">
-                      <span class="Modal--Hours--Item--Day">${this.dic.t(`Pages.Maps.day.${i}`)}</span>
-                      <span class="Modal--Hours--Item--Time">${this.dic.t('Pages.Maps.closed')}</span>
-                    </li>
-                  `;
-                }
-              })
-              .join('')}</ul>
-            ${store.email ? `<a href="mailto:${store.email}">${store.email}</a>` : ''}
-            ${store.tel ? `<a href="tel:${store.tel}">${store.tel}</a>` : ''}
-            <ul>
-              ${Object.keys(store.media)
-                .map(
-                  (key: string): string => {
-                    return store.media[key]
-                      ? `
-                        <li>
-                          <a target='__blank' class="Modal--Media Modal--Media__Active" href="${store.media[key]}">${
-                          key[0]
-                        }</a>
-                        </li>
-                      `
-                      : `
-                        <li>
-                          <div class="Modal--Media">${key[0]}</div>
-                        </li>
-                      `;
-                  },
-                )
-                .join('')}
-            </ul>
-            <ul>
-              ${Object.keys(store.services)
-                .map(
-                  (key: string): string => {
-                    return store.services[key]
-                      ? `<li><div class="Modal--Media Modal--Media__Active">${key[0]}</div></li>`
-                      : `<li><div class="Modal--Media">${key[0]}</div></li>`;
-                  },
-                )
-                .join('')}
-            </ul>
-          </div>
-        </div>
-      `;
-    });
-
     if (this.props.onClick) {
       this.marker.on('click', this.props.onClick);
     }
