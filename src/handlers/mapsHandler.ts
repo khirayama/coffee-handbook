7d8635bc4ef6a66e5d968c59f7e6737fd74d47db
Kotaro Hirayama <just.do.it.my.life@gmail.com>
2018-09-04 21:32:49 +0900

    Update mapsHandlers

--- src/handlers/mapsHandler.ts
+++ src/handlers/mapsHandler.ts
@@ -1,10 +1,28 @@
 import * as express from 'express';
 
 import { IMapsPage } from 'presentations/pages/Maps';
+import { getNextStatusMessage } from 'presentations/utils/getNextStatusMessage';
+import { getOpenStatus, IOpenStatus } from 'presentations/utils/getOpenStatus';
+import { IStore, Store } from 'resources/Store';
 import { Dictionary } from 'utils/Dictionary';
 
 export function mapsHandler(req: express.Request, res: express.Response): void {
   const dic: Dictionary = req.dic;
+  const store: IStore = Store(req.lang)
+    .where({
+      key: req.query.key,
+    })
+    .findOne();
+
+  const now: Date = new Date();
+  const currentDay: number = now.getDay();
+  let openStatus: IOpenStatus | null = null;
+  let nextStatusMessage: string | null = null;
+  if (store) {
+    openStatus = getOpenStatus(now, store.hours);
+    nextStatusMessage = getNextStatusMessage(openStatus, dic);
+  }
+
   const props: IMapsPage = {
     ...req.layout,
     title: dic.t('name'),
@@ -13,12 +31,10 @@ export function mapsHandler(req: express.Request, res: express.Response): void {
     image: 'TODO',
     pageType: 'cafe',
 
-    header: {
-      lang: req.lang,
-    },
-    navigation: {
-      path: req.originalUrl,
-    },
+    store,
+    currentDay,
+    openStatus,
+    nextStatusMessage,
   };
 
   res.render('pages/Maps', { dic, props });
