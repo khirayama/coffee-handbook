7787390554817c10027c2e4e3b05a115f7e4e2da
Kotaro Hirayama <just.do.it.my.life@gmail.com>
2018-09-08 09:42:32 +0900

    Remove note in StoreCard

--- src/presentations/components/StoreCard.ts
+++ src/presentations/components/StoreCard.ts
@@ -1,16 +1,5 @@
 import { View } from 'presentations/components/View';
 
-// - [ ] 開閉アイコン
-// - [ ] 電話アイコン
-// - [ ] メールアイコン
-// - [ ] webアイコン
-// - [ ] ecアイコン
-// - [ ] facebookアイコン
-// - [ ] twitterアイコン
-// - [ ] instagramアイコン
-// - [ ] instagramTagアイコン
-// - [ ] googleMapsアイコン
-
 export class StoreCard extends View {
   public setEventListeners(): void {
     this.$el.find('.StoreCard--Content--OpenStatus').on('click', this.toggleHours.bind(this));
