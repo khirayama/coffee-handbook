2d56796f3f32c14741e4d83b6f9230374ced283c
Kotaro Hirayama <just.do.it.my.life@gmail.com>
2018-09-24 10:45:20 +0900

    Update hot coffee recipe

--- src/data/recipes/coffeeHotRecipe.ts
+++ src/data/recipes/coffeeHotRecipe.ts
@@ -43,7 +43,7 @@ export const coffeeHotRecipe: IRawPost<IRawRecipe> = {
         },
         note: {
           ja: '中細挽き',
-          en: 'Middle casdcadscasdcasdcas',
+          en: 'Medium Fine',
         },
       },
       {
@@ -52,8 +52,8 @@ export const coffeeHotRecipe: IRawPost<IRawRecipe> = {
           en: 'Water',
         },
         quantity: {
-          ja: '200ml',
-          en: '200ml',
+          ja: '400ml',
+          en: '400ml',
         },
         note: {
           ja: '94℃程度',
@@ -64,30 +64,66 @@ export const coffeeHotRecipe: IRawPost<IRawRecipe> = {
     steps: [
       {
         summary: {
-          ja: '水をあれあれ',
-          en: 'Water Water',
+          ja: '準備',
+          en: 'Prepares',
         },
         description: {
-          ja: 'いろいろ細かく',
-          en: 'Do something',
-        },
-        note: {
-          ja: 'ほげほげ',
-          en: 'hogehoge',
+          ja: `
+            水200mlを94℃程度まで沸かし、抽出器具にセットしたペーパーフィルターに湯通しする。<br>
+            豆は中細挽きにして、湯通ししたペーパーフィルターにセットする。
+          `,
+          en: `
+            Boil water to 21℉ and parboil the paper filter.<br>
+            Graind the beans and set it to the filter parboilded.
+          `,
         },
       },
       {
         summary: {
-          ja: '水をあれあれ',
-          en: 'Water Water',
+          ja: '抽出',
+          en: 'Brew',
         },
         description: {
-          ja: 'いろいろ細かく',
-          en: 'Do something',
+          ja: `
+            お湯を75ml注ぎ、45秒蒸らす。その後、50mlずつに6回に分けながら注ぐ。合計375ml使用。<br>
+            最後のお湯が落ちきる前にドリッパーを取り上げる。
+          `,
+          en: `
+            First, pours 75ml water up to the dripper. After this, pours 50ml water 6 separate times. Use 375ml in all.<br>
+            Remove the dripper before last time dropping.
+          `,
         },
         note: {
-          ja: 'ほげほげ',
-          en: 'hogehoge',
+          ja: `
+            フィルターに直接お湯を注がないように注意する。コーヒー豆を通らないお湯が混ざってしまう。
+          `,
+          en: `
+            Be careful not to pour the water directly to the filter because hot water does not pass through coffee beans.
+          `,
+        },
+      },
+      {
+        summary: {
+          ja: '出来上がり！',
+          en: "It's ready to drink! ",
+        },
+        description: {
+          ja: `
+            お湯を75ml注ぎ、45秒蒸らす。その後、50mlずつに6回に分けながら注ぐ。合計375ml使用。<br>
+            最後のお湯が落ちきる前にドリッパーを取り上げる。
+          `,
+          en: `
+            First, pours 75ml water up to the dripper. After this, pours 50ml water 6 separate times. Use 375ml in all.<br>
+            Remove the dripper before last time dropping.
+          `,
+        },
+        note: {
+          ja: `
+            フィルターに直接お湯を注がないように注意する。コーヒー豆を通らないお湯が混ざってしまう。
+          `,
+          en: `
+            Be careful not to pour the water directly to the filter because hot water does not pass through coffee beans.
+          `,
         },
       },
     ],
