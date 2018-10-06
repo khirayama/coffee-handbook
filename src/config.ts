f9a37992f2d4b7de1b4b1d15c4d2d9e3d32f092e
Kotaro Hirayama <just.do.it.my.life@gmail.com>
2018-10-07 07:33:06 +0900

    Create about page

--- src/config.ts
+++ src/config.ts
@@ -1,6 +1,8 @@
 export const config: {
   url: string;
   languages: string[];
+  githubUrl: string;
+  patreonUrl: string;
   facebookAppId: string;
   facebookPageUrl: string;
   twitterCardType: string;
@@ -8,6 +10,10 @@ export const config: {
 } = {
   url: process.env.NODE_ENV === 'production' ? 'https://coffeehandbook.app' : '//localhost:3030',
   languages: ['en', 'ja'],
+  // TODO: Update repo name
+  githubUrl: 'https://github.com/khirayama/tmp',
+  // TODO: Get patreon https://www.patreon.com/
+  patreonUrl: '',
   facebookAppId: 'test id',
   facebookPageUrl: 'facebook page url',
   twitterCardType: 'summary_large_image',
