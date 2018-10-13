60e35ba784533db8eec282292fae5a1a2286045e
Kotaro Hirayama <just.do.it.my.life@gmail.com>
2018-10-13 23:57:33 +0900

    Update config

--- src/config.ts
+++ src/config.ts
@@ -8,14 +8,13 @@ export const config: {
   twitterCardType: string;
   twitterAccount: string;
 } = {
-  url: process.env.NODE_ENV === 'production' ? 'https://coffeehandbook.app' : '//localhost:3030',
+  url: process.env.NODE_ENV === 'production' ? 'https://coffee-handbook.com' : '//localhost:3030',
   languages: ['en', 'ja'],
-  // TODO: Update repo name
-  githubUrl: 'https://github.com/khirayama/tmp',
+  githubUrl: 'https://github.com/khirayama/coffee-handbook',
   // TODO: Get patreon https://www.patreon.com/
   patreonUrl: '',
   facebookAppId: 'test id',
   facebookPageUrl: 'facebook page url',
   twitterCardType: 'summary_large_image',
-  twitterAccount: '@coffee_handbook',
+  twitterAccount: '@coffeehandbook_',
 };
