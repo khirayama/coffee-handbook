0a0c491b30b12ec151ef118152530353c4df2192
Kotaro Hirayama <just.do.it.my.life@gmail.com>
2018-11-01 08:19:41 +0900

    Remove needless fields for config

--- src/config.ts
+++ src/config.ts
@@ -2,8 +2,6 @@ export const config: {
   url: string;
   languages: string[];
   githubUrl: string;
-  patreonUrl: string;
-  facebookAppId: string;
   facebookPageUrl: string;
   twitterCardType: string;
   twitterAccount: string;
@@ -12,11 +10,8 @@ export const config: {
   url: process.env.NODE_ENV === 'production' ? 'https://coffee-handbook.com' : '//localhost:3030',
   languages: ['en', 'ja'],
   githubUrl: 'https://github.com/khirayama/coffee-handbook',
-  // TODO: Get patreon https://www.patreon.com/
-  patreonUrl: '',
-  facebookAppId: '',
   facebookPageUrl: 'https://www.facebook.com/coffeehandbook/',
-  twitterCardType: 'summary_large_image',
+  twitterCardType: 'summary',
   twitterAccount: '@coffeehandbook_',
   instagramAccount: 'coffeehandbook',
 };
