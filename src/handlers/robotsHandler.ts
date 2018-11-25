2a53a6f37608c94c0f000e1cc35895d830f2271a
Kotaro Hirayama <just.do.it.my.life@gmail.com>
2018-11-26 08:37:58 +0900

    Update robotsHandler

--- src/handlers/robotsHandler.ts
+++ src/handlers/robotsHandler.ts
@@ -6,10 +6,6 @@ export function robotsHandler(req: express.Request, res: express.Response): void
   const lang: string = req.lang;
 
   const robotsContent: string = `User-agent: *
-  Disallow: /*.gif$
-  Disallow: /*.png$
-  Disallow: /*.jpg$
-
   Sitemap: ${config.url[lang]}/sitemap.xml`;
 
   res.set('Content-Type', 'text/plain').send(robotsContent);
