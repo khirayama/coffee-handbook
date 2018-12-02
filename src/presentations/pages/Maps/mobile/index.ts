3a0bc48bbaaf2877184bb54cc8f287249ede4eb7
Kotaro Hirayama <just.do.it.my.life@gmail.com>
2018-12-03 06:28:36 +0900

    Remove done todo

--- src/presentations/pages/Maps/mobile/index.ts
+++ src/presentations/pages/Maps/mobile/index.ts
@@ -9,7 +9,6 @@ import { loadView, reducer } from 'presentations/pages/Maps/reducer';
 import { storeSearchEngine } from 'StoreSearchEngine';
 import { Store as AppStore } from 'utils/Store';
 
-// TODO: store検索
 // TODO: storeをshopに変更
 // TODO: ActorModelでの実装を検討。特に検索 & レコメンドの処理が重い可能性高く、worker thread感がる必要あるかも
 declare global {
