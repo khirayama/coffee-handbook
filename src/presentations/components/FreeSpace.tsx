b54ad66e782ad0ffcccd8e16ee9dbf8a9a4fb265
Kotaro Hirayama <just.do.it.my.life@gmail.com>
2018-12-06 07:18:08 +0900

    Remove needless lines

--- src/presentations/components/FreeSpace.tsx
+++ src/presentations/components/FreeSpace.tsx
@@ -4,20 +4,6 @@ export class FreeSpace extends React.Component<{}, {}> {
   public render(): JSX.Element {
     return (
       <div className="FreeSpace">
-        <ul className="FreeSpace--ConditionList">
-          <li className="FreeSpace--ConditionList--Item">
-            開店中の
-            <br />
-            近くの
-          </li>
-          <li className="FreeSpace--ConditionList--Item">こだわり</li>
-          <li className="FreeSpace--ConditionList--Item">仕事に</li>
-          <li className="FreeSpace--ConditionList--Item">
-            ペット
-            <br />
-            と一緒に
-          </li>
-        </ul>
         <div className="FreeSpace--Ad">
           <div className="FreeSpace--Ad--Content">Advertisingt Space</div>
         </div>
