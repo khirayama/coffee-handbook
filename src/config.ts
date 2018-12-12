4040b577767797e9471d1f313563b152dd252423
Kotaro Hirayama <just.do.it.my.life@gmail.com>
2018-12-12 09:07:53 +0900

    Add comments

--- src/config.ts
+++ src/config.ts
@@ -1,4 +1,8 @@
 // tslint:disable:no-http-string
+// FYI:
+// サポートされている言語または地域のコード: https://support.google.com/webmasters/answer/189077?hl=ja
+// ISO639-1: https://ja.wikipedia.org/wiki/ISO_639-1%E3%82%B3%E3%83%BC%E3%83%89%E4%B8%80%E8%A6%A7
+// ISO3166-1: https://ja.wikipedia.org/wiki/ISO_3166-1_alpha-2
 export const config: {
   url: {
     en: string;
