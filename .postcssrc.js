200acd09885b4ade069a5a017d36583ab343a1f8
Kotaro Hirayama <just.do.it.my.life@gmail.com>
2019-02-28 11:12:32 +0900

    Remove needless link

--- .postcssrc.js
+++ .postcssrc.js
@@ -8,7 +8,6 @@ module.exports = {
     'postcss-simple-vars': null,
     'postcss-nested': null,
     autoprefixer: {
-      // https://wiki.linecorp.com/pages/viewpage.action?pageId=263110918
       // https://github.com/ai/browserslist#queries
       browsers: ['last 2 versions', 'ie >= 9', 'iOS >= 8', 'Android >= 4'],
     },
